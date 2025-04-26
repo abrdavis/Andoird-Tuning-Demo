061c9a021eb9bc33d223825e7bda788b16c4255a
adam davis <adamDavis@METATAG>
2025-04-26 11:05:01 -0400

    Initial commit

---
+++ app/src/main/java/com/raveldev/timer/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.raveldev.timer
+
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.snackbar.Snackbar
+import com.raveldev.timer.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    val PERMISSION_REQUEST_CODE: Int = 200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var recordAudioGranted = ContextCompat.checkSelfPermission(this, "android.permission.RECORD_AUDIO")
+
+        if(recordAudioGranted != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(RECORD_AUDIO),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE
+
+                -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                } else {
+
+                }
+                return
+            }
+
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/raveldev/timer/TunerFragment.kt
@@ -0,0 +1,161 @@
+package com.raveldev.timer
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import be.tarsos.dsp.AudioDispatcher
+import be.tarsos.dsp.AudioProcessor
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchProcessor
+import com.raveldev.timer.databinding.FragmentTunerBinding
+import kotlin.math.abs
+import kotlin.math.pow
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class TunerFragment : Fragment() {
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentTunerBinding? = null
+
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentTunerBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        val noteTextView : TextView = binding.textNote
+
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        try{
+            generateFrequencyMap()
+            var recordAudioGranted = ContextCompat.checkSelfPermission(this.requireContext(), "android.permission.RECORD_AUDIO")
+            if(recordAudioGranted == PackageManager.PERMISSION_GRANTED){
+                val dispatcher: AudioDispatcher =
+                    AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
+
+                val pdh = PitchDetectionHandler { result, e ->
+                    try{
+                        val pitchInHz = result.pitch
+                        val closetNote = getClosestNoteString(0, FREQUENCY_LIST.size -1, result.pitch);
+
+                        //given our pitch in hz, which value in our map of notes is closest
+                        activity?.runOnUiThread(Runnable {
+                            noteTextView.text = closetNote
+                            textView.text = "${pitchInHz}"
+                        })
+
+                    }
+                    catch(e: Exception){
+
+                    }
+
+                }
+
+                val p: AudioProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050f, 1024, pdh)
+                dispatcher.addAudioProcessor(p)
+                Thread(dispatcher, "Audio Dispatcher").start()
+            }
+            else{
+
+            }
+        }
+        catch(e: Exception){
+            Log.e("tuner", e.message.toString())
+        }
+        return root
+
+    }
+    val BASE_FREQUENCY_HZ = 440.0;
+    val TWELTH_ROOT_TWO = 1.059463094359
+    val BASE_NOTE_STR = "A4"
+    //A4
+    val BASE_NOTE = 81
+    val STARTING_NOTE = 0;
+    val HIGHEST_NOTE = 127
+    val NOTE_ORDER = arrayOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    var FREQUENCY_LIST  = mutableListOf<Float>()
+    val MAX_NOTE_ORDER = 11;
+    val STARTING_OCTAVE = -2;
+    var arrayIndextoNote = mutableMapOf<Int, String>()
+    //C0 is in the "-2" octave in MIDI
+    private fun generateFrequencyMap(){
+        //C0(-2)
+        var noteOrderIndex = 0
+        var startingNote = 0
+        var currentOctave = STARTING_OCTAVE
+        while(startingNote <= HIGHEST_NOTE){
+            val arrayIndex = "${NOTE_ORDER[noteOrderIndex]}${currentOctave}"
+            var noteValue =  generateFrequencyForNoteNumber(startingNote)
+            arrayIndextoNote[startingNote] =arrayIndex
+            FREQUENCY_LIST.add(noteValue.toFloat())
+            startingNote += 1
+            noteOrderIndex +=1
+            if(noteOrderIndex == 12){
+                noteOrderIndex = 0;
+                currentOctave += 1;
+            }
+
+        }
+
+    }
+    private fun getClosestNoteString(
+        start: Int,
+        end: Int,
+        myNumber: Float
+    ): String {
+        val mid = (start + end) / 2
+        if (FREQUENCY_LIST[mid] == myNumber) return arrayIndextoNote[mid]!!
+        if (start == end - 1) return if (abs(FREQUENCY_LIST[end] - myNumber) >= abs(
+                FREQUENCY_LIST[start] - myNumber
+            )
+        ) arrayIndextoNote[start]!! else arrayIndextoNote[end]!!
+        return if (FREQUENCY_LIST[mid] > myNumber) getClosestNoteString(
+            start,
+            mid,
+            myNumber
+        ) else getClosestNoteString(mid, end, myNumber)
+    }
+
+    private fun generateFrequencyForNoteNumber(noteNumber :Int ) : Double {
+        var result = 0.0
+        val powerToRaise = (noteNumber - BASE_NOTE)
+        result = BASE_FREQUENCY_HZ * (TWELTH_ROOT_TWO.pow(powerToRaise))
+
+        return result
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
